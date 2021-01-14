1a5cd9ce3f8605cba33ad3264b8f11f8ab397bdf
Taiga <mokuzuzzz@gmail.com>
2021-01-14 22:52:25 +1000

    chore: add index

---
+++ index.ts
@@ -0,0 +1,36 @@
+import {Socket} from "socket.io";
+
+const app = require("express")();
+const http = require("https").Server(app);
+
+const io = require("socket.io")(http, {
+  origin: "https://enc-chat.vercel.app",
+  cors: {
+    origin: "https://enc-chat.vercel.app",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+const cors = require("cors");
+
+app.use(cors());
+
+const PORT = 8000;
+io.on("connection", (socket: Socket) => {
+  console.log("CONNECTED");
+  socket.on("join-room", (roomId, user) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("join-room", user);
+  });
+  socket.on("send-message", (roomId, sender, message) => {
+    console.log(`roomId: ${roomId.length}`);
+    console.log(`encrypted sender: ${sender}`);
+    console.log(`encrypted message: ${message}`);
+    socket.to(roomId).emit("new-message", sender, message);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
